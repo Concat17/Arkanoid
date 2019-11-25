906547a18e9facf3cbaf56e7ba02323ede5be66a
levabala <levabala@ya.ru>
2019-11-25 21:39:06 +0300

    a lot of comments and suggestions

--- src/Ball.ts
+++ src/Ball.ts
@@ -13,16 +13,24 @@ export class Ball {
     this.color = color;
   }
 
+  // name mismatch? my "move" method performs drawing?
   move(ctx: CanvasRenderingContext2D): void {
+    // it would be better to depend moving on global Time
+    // then your system will not depend on fps
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = this.color;
+
+    // placing this code inside ctx brackets is not good as i've already said
     this.x += this.velocity * this.direction[0];
     this.y += this.velocity * this.direction[1];
     ctx.fill();
     ctx.closePath();
   }
 
+  // a little naming mismatch
+  // here besides "border checking" u also do direction reverting
+  // so it's better to name it smth like "processBorderCollision" or "processBounce"
   checkBorder(canvasWidth: number, canvasHeight: number): void {
     if (
       this.x + this.velocity > canvasWidth - this.radius ||
